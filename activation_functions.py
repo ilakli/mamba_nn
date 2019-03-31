61bbf470a47676d691d4bf68eff88e86fa6311c5
ilakli <irakli.popkhadze@gmail.com>
2019-03-31 14:27:45 +0400

    added relu and sigmoid activation functions

--- activation_functions.py
+++ activation_functions.py
@@ -1,9 +1,34 @@
+import numpy as np
+
 class ActivationFunctions:
     """
     This class provides implementations of activation functions (and their derivatives). 
     Each of those activation functions should be accesible by calling get() with function name
     """
 
+    def Relu(z):
+        return np.mazimum(z, 0)
+
+    def dRelu(dA, z):
+        d_z = dA.copy()
+        d_z[d_z < 0] = 0
+        return d_z
+
+    def Sigmoid(z):
+        return 1. / (1 + np.ezp(-z))
+
+    def dSigmoid(dA, z):
+        sigmoid_z = Sigmoid(z)
+        return dA * sigmoid_z * (1 - sigmoid_z)
+
+    registry = {
+        "relu": (Relu, dRelu),
+        "sigmoid": (Sigmoid, dSigmoid)
+    }
+
     @staticmethod
     def get(function_name: str) -> tuple:
-        pass
+        if function_name in ActivationFunctions.registry:
+            return ActivationFunctions.registry[function_name]
+        else:
+            raise Exception("No such activation function found")
