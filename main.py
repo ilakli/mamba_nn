207e74d040e5f1015ffb5f82420c481537045eae
ilakli <irakli.popkhadze@gmail.com>
2019-05-02 18:12:36 +0400

    added different splitting functions

--- main.py
+++ main.py
@@ -28,6 +28,12 @@ def d_quadratic_weight_function(W, x, dz):
 def splitting_function_0(X):
     return np.zeros((X.shape[1]))
 
+def splitting_function_mean(X):
+    return (np.mean(X, axis=0) > 0.5).astype(int)
+
+def splitting_function_even_odd(X):
+    return np.arange(X.shape[1]) % 2
+
 def main():
 
     kobi = MambaNet(24)
@@ -43,16 +49,16 @@ def main():
                        1,
                        True)
 
-    layer1 = BaseLayer(128, "relu", "xavier",
+    layer1 = BaseLayer(128, "sigmoid", "tupoi",
                        (linear_weight_function, d_linear_weight_function),
-                       2,
+                       1,
                        False,
                        0.0005)
-    # layer2 = BaseLayer(256, "relu", "xavier",
-    #                    (linear_weight_function, d_linear_weight_function),
-    #                    1,
-    #                    True)
-    layer2 = BasePieceWiseLayer([layer_21, layer_22], splitting_function_0)
+    layer2 = BaseLayer(64, "relu", "xavier",
+                       (linear_weight_function, d_linear_weight_function),
+                       1,
+                       True)
+    # layer2 = BasePieceWiseLayer([layer_21, layer_22], splitting_function_even_odd)
     layer3 = BaseLayer(10, "relu", "xavier",
                        (linear_weight_function, d_linear_weight_function),
                        1,
@@ -65,7 +71,7 @@ def main():
 
     file_names = ["data_batch_%s" % (str(ind)) for ind in range(1, 6)]
 
-    kobi.train_from_files(file_names, "test_batch", get_cifar_dataset, learning_rate=0.01, n_epochs=10)
+    kobi.train_from_files(file_names, "test_batch", get_cifar_dataset, learning_rate=0.05, n_epochs=50)
 
     # print (kobi.layers[1].average_output / kobi.layers[1].number_of_examples)
 
