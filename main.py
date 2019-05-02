652a47b338be896037114c370b14a6456c19a4e5
xosrika <gkhos15@freeuni.edu.ge>
2019-05-02 22:26:58 +0400

    piecewise works

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def splitting_function_0(X):
     return np.zeros((X.shape[1]))
 
 def splitting_function_mean(X):
-    return (np.mean(X, axis=0) > 0.5).astype(int)
+    return (np.mean(X, axis=0) > 0).astype(int)
 
 def splitting_function_even_odd(X):
     return np.arange(X.shape[1]) % 2
@@ -49,16 +49,17 @@ def main():
                        1,
                        True)
 
-    layer1 = BaseLayer(128, "sigmoid", "tupoi",
-                       (linear_weight_function, d_linear_weight_function),
-                       1,
-                       False,
-                       0.0005)
+    layer1 = BasePieceWiseLayer([layer_21, layer_22], splitting_function_mean)
+    # layer1 = BaseLayer(128, "sigmoid", "tupoi",
+    #                    (linear_weight_function, d_linear_weight_function),
+    #                    2,
+    #                    False,
+    #                    0.0005)
     layer2 = BaseLayer(64, "relu", "xavier",
                        (linear_weight_function, d_linear_weight_function),
                        1,
                        True)
-    # layer2 = BasePieceWiseLayer([layer_21, layer_22], splitting_function_even_odd)
+    # layer2 = BasePieceWiseLayer([layer_21, layer_22], splitting_function_1)
     layer3 = BaseLayer(10, "relu", "xavier",
                        (linear_weight_function, d_linear_weight_function),
                        1,
