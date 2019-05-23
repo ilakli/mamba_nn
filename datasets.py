c8a4e2e4bff20d2355f568bb9382a5d8b3972ea3
ilakli <irakli.popkhadze@gmail.com>
2019-05-23 17:53:03 +0400

    added fashion mnist dataset

--- datasets.py
+++ datasets.py
@@ -2,6 +2,7 @@ import pickle
 import random
 import numpy as np 
 import os.path
+import csv
 
 def create_simple_dataset(*args):
     random.seed(0)
@@ -39,3 +40,31 @@ def get_cifar_dataset(batch_name, *args):
     cifar_x_standardized = cifar_x_mean0 / np.std(cifar_x, axis=0)
 
     return cifar_x_standardized, cifar_y
+
+def get_fashion_mnist_dataset(data_path):
+    # train_path = os.path.join(data_dir, 'train.csv')
+    # test_path = os.path.join(data_dir, 'test.csv')
+
+    mnist_x = []
+    mnist_y = []
+
+    with open(data_path) as test_csv:
+        csv_reader = csv.reader(test_csv, delimiter=',', )
+        
+        for index, row in enumerate(test_csv):
+            if index == 0: continue
+
+            row_data = list(map(int, row.strip().split(',')))
+
+            label = row_data[0]
+            features = np.array(row_data[1:])
+
+            mnist_x.append(features)
+            mnist_y.append(label)
+
+    mnist_x = np.array(mnist_x).T
+
+    mnist_x_mean0 = mnist_x - np.mean(mnist_x, axis=0)
+    mnist_x_standardized = mnist_x_mean0 / np.std(mnist_x, axis=0)
+
+    return mnist_x_standardized, mnist_y
